b3a9f62a242276ea5037864ac5c8ea99d9dad14f
janusz10007 <48623077+janusz10007@users.noreply.github.com>
2020-11-19 16:48:27 +0100

    spojROL

---
+++ 723rol/723rol/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _723rol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for(int i=0;i<t;i++)
+            {
+                Queue<string> q = new Queue<string>(Console.ReadLine().Split(' '));
+                q.Dequeue();
+                var item = q.Dequeue();
+                q.Enqueue(item);
+
+                foreach(var x in q)
+                {
+                    Console.Write(x + " ");
+                }
+                Console.WriteLine();
+
+
+            }
+        }
+    }
+}
